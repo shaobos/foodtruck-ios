82795f4100bab55c1aff93f6d4f271af4e82ea14
Shaobo Sun <gogoshaobo@gmail.com>
2015-05-10 16:02:22 -0700

    A-Z indexes work now for truck table view

--- Food Truck Demo/TrucksTableViewController.swift
+++ Food Truck Demo/TrucksTableViewController.swift
@@ -7,23 +7,58 @@
 //
 
 
-class TrucksTableViewController : UIViewController, UITableViewDelegate{
+// need uppercase function for String
+// http://stackoverflow.com/q/26245645/677596
+import Foundation
+
+class TrucksTableViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+//    private let titlesBySection = ["A" : ["Ac", "Ad"], "B": ["bc"], "C": ["cc"]]
+//    private let sections = ["A", "B", "C"]
     private let reuseIdentifier = "TruckCell"
+    private var trucksIndexedByInitial = [String: [String]]()
+    private var trucksSectionHeader = [String]()
+
+    
     private var currentTruckId:String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        convertTruckData(Trucks.trucks)
     }
 
     func getTruckByIndexPath(indexPath: NSIndexPath) -> [String:String] {
-        var truck = (Array(TheTrucks.trucks.values)[indexPath.row])
+        var truck = (Array(Trucks.trucks.values)[indexPath.row])
         return truck
     }
     
+    func convertTruckData(input: [String:[String: String]]) {
+        for truck in input.values {
+            var name:String = truck["name"]!
+            var truckId:String = truck["id"]!
+            var index = advance(name.startIndex, 0)
+            var firstCharacter = String(name[index]) // character to string here to avoid annoying conversion
+            if trucksIndexedByInitial[firstCharacter] == nil {
+                trucksIndexedByInitial[firstCharacter] = [String]()
+            }
+            trucksIndexedByInitial[firstCharacter]!.insert(truckId, atIndex: 0)
+        }
+        
+        for character in Array(trucksIndexedByInitial.keys).sorted(<) {
+            
+            trucksSectionHeader.append(String(character))
+        }
+    }
+    
+    /*
+        how does each cell look like
+    */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var customCell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier) as TruckTableCellCustomView
-
-        var truck = getTruckByIndexPath(indexPath)
-        var id = truck["id"] as String!
+        var header = trucksSectionHeader[indexPath.section]
+        var trucks = trucksIndexedByInitial[header]
+        var id = trucks![indexPath.row]
+        var truck = Trucks.trucks[id]!
         if let theImage: Image = Images.truckImages[id] {
             customCell.truckImage.image =  theImage.image
         }
@@ -33,8 +68,11 @@ class TrucksTableViewController : UIViewController, UITableViewDelegate{
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        var truck = getTruckByIndexPath(indexPath)
-        currentTruckId = truck["id"]
+//        var truck = getTruckByIndexPath(indexPath)
+        var header = trucksSectionHeader[indexPath.section]
+        var trucks = trucksIndexedByInitial[header]
+        currentTruckId = trucks![indexPath.row]
+
         performSegueWithIdentifier("TruckTableToDetail", sender: nil)
     }
 
@@ -43,8 +81,42 @@ class TrucksTableViewController : UIViewController, UITableViewDelegate{
         controller.setTruckId(currentTruckId!)
         // TODO: revisit shortly
     }
-    
+
+    /*
+        Number Of Rows In Section
+    */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TheTrucks.trucks.count
+        var sectionTitle = trucksSectionHeader[section]
+        return trucksIndexedByInitial[sectionTitle]!.count
     }
+    
+    func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
+        var i:Int = 0
+        println(index)
+        for head in trucksSectionHeader {
+            if head == title {
+                println("Found the title!")
+                return i
+            }
+            i++
+        }
+        return 0
+    }
+    
+    func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+        return trucksSectionHeader
+    }
+    /*
+        decide how many sections
+    */
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return trucksSectionHeader.count
+    }
+    
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return trucksSectionHeader[section]
+    }
+    
+    
 }
